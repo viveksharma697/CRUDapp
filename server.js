f5afc13bdb649b4da2c535539d52b196a2bb0c69
viveksharma697 <viveksharma697@gmail.com>
2022-06-03 15:23:55 +0530

    CRUD app completed

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const bodyparser = require('body-parser');
+const bodyparser = require("body-parser");
 const path = require('path');
-const ejs = require('ejs')
 
-const app = express()
+const connectDB = require('./server/database/connection');
 
-dotenv.config({path:'config.env'})
+const app = express();
+
+dotenv.config( { path : 'config.env'} )
 const PORT = process.env.PORT || 8080
 
+// log requests
 app.use(morgan('tiny'));
 
-app.use(bodyparser.urlencoded({extended:true}))
+// mongodb connection
+connectDB();
 
-app.set("view engine", "ejs");
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended : true}))
 
-//  load assets
-app.use('/css',express.static(path.resolve(__dirname, "assets/css")))
-app.use('/img',express.static(path.resolve(__dirname, "assets/img")))
-app.use('/js',express.static(path.resolve(__dirname, "assets/js")))
+// set view engine
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-app.listen(PORT, ()=> {console.log(`Server is running on http://localhost:${PORT}`)});
+// load routers
+app.use('/', require('./server/routes/router'))
+
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
