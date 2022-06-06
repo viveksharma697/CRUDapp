cb991cd3a44af65843f0f2a3cc9c15dfa6fdee10
viveksharma697 <viveksharma697@gmail.com>
2022-06-06 12:04:57 +0530

    User Management system created by CRUD

--- server/database/connection.js
+++ server/database/connection.js
@@ -6,8 +6,8 @@ const connectDB = async () => {
         const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
+            // useFindAndModify: false,
+            // useCreateIndex: true
         })
 
         console.log(`MongoDB connected : ${con.connection.host}`);
